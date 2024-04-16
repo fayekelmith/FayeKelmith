ea143a50ffe2540b2082da7f129c92516b0d19bf
fayekelmith <faye.12222883@lpu.in>
2024-04-16 20:59:28 +0530

    essay primary page setup

---
+++ app/(pages)/main-page-components/EssayCard.tsx
@@ -0,0 +1,45 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
+interface EssayCardProps {
+  title: string;
+  description: string;
+  imgurl: string;
+}
+import Image from "next/image";
+const EssayCard = (props: EssayCardProps) => {
+  return (
+    <Card className="p-8 bg-slate-100 shadow-sm hover:shadow-lg duration-700">
+      <div className="flex gap-x-8 items-center">
+        <CardContent>
+          <Image
+            src={props.imgurl}
+            alt="Essay Image"
+            width={150}
+            height={150}
+          />
+        </CardContent>
+        <CardContent>
+          <CardDescription>
+            <h2>{props.title}</h2>
+            <p>
+              {props.description.length > 100
+                ? props.description.slice(0, 100) + "..."
+                : props.description}
+            </p>
+            <Button className="items-end">Read More</Button>
+          </CardDescription>
+        </CardContent>
+      </div>
+      <Separator orientation="horizontal" />
+    </Card>
+  );
+};
+
+export default EssayCard;
